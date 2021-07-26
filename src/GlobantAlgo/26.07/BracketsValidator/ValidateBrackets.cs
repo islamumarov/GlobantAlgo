c4d587bb8c5d29bd87c92748f3cf4808b0417497
Islam <Ihtyyarovich@gmail.com>
2021-07-26 14:54:52 +0300

    added unit test to the first task

--- src/GlobantAlgo/26.07/BracketsValidator/ValidateBrackets.cs
+++ src/GlobantAlgo/26.07/BracketsValidator/ValidateBrackets.cs
@@ -37,29 +37,5 @@ namespace BracketsValidator
             return stack.Count == 0;
         }
 
-        public bool IsBracketsValid(string bracketsLine)
-        {
-            if (bracketsLine == null || bracketsLine.Length == 0)
-                return false;
-            if (bracketsLine.Length == 1)
-                return false;
-            var brackets = bracketsLine.ToCharArray();
-            var openBrackets = OpenBrackets;
-            var closeBrackets = CloseBrackets;
-            var stack = new Stack<char>();
-            for (int i = 0; i < brackets.Length; i++)
-            {
-                if (openBrackets.Contains(brackets[i]))
-                    stack.Push(brackets[i]);
-                if (closeBrackets.Contains(brackets[i]))
-                {
-                    if (stack.Count == 0)
-                        return false;
-                    if (stack.Pop() != brackets[i])
-                        return false;
-                }
-            }
-            return stack.Count == 0;
-        }
     }
 }
--- src/GlobantAlgo/26.07/ProjectWithTests/ValidateBracketsTest.cs
+++ src/GlobantAlgo/26.07/ProjectWithTests/ValidateBracketsTest.cs
@@ -1,22 +1,25 @@
-﻿using BracketsValidator;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BracketsValidator;
 using Xunit;
 
-namespace Tests
+namespace ProjectWithTests
 {
     public class ValidateBracketsTest
     {
-
         [Theory]
         [InlineData("{[()]}", true)]
         [InlineData("{[(])}", false)]
         public void BracketsLineIsValidTest(string bracketsLine, bool expect)
         {
             var validate = new ValidateBrackets();
-            var actual = validate.IsBracketsValid(bracketsLine);
+            var actual = validate.IsValidBrackets(bracketsLine);
 
             Assert.Equal(expect, actual);
 
         }
-
     }
 }
