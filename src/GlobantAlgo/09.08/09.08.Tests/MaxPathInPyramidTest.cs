7f806428614f7924f63c27b1dbbd3135a0f4eb3d
Islam <Ihtyyarovich@gmail.com>
2021-08-14 20:04:45 +0300

    added 2 task solution

---
+++ src/GlobantAlgo/09.08/09.08.Tests/MaxPathInPyramidTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PyramidAndNodes
+{
+    public class MaxPathInPyramidTest
+    {
+        [Fact]
+        public void maxPathSumTest()
+        {
+            var expected = 143;
+            int[,] triangle = { { 1, 0, 0, 0 }, { 10, 15, 0, 0 }, { 2, 5, 27, 0 }, { 99, 3, 100, 6 } };
+            int actual = Program.maxPathSum(triangle, 3,3);
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ src/GlobantAlgo/09.08/PyramidAndNodes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PyramidAndNodes
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] triangle = { { 1, 0, 0, 0 }, { 10, 15, 0, 0 }, { 2, 5, 27, 0 }, { 99, 3, 100, 6 } };
+            var result = maxPathSum(triangle, 3, 3);
+            Console.WriteLine(result);
+        }
+
+        public static int maxPathSum(int[,] tri,
+                          int m, int n)
+        {
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (tri[i + 1, j] >
+                           tri[i + 1, j + 1])
+                        tri[i, j] +=
+                               tri[i + 1, j];
+                    else
+                        tri[i, j] +=
+                           tri[i + 1, j + 1];
+                }
+            }
+            return tri[0, 0];
+        }
+    }
+}
