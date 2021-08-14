702dfe9d0fd2b290cdc0cc93348c0ea5d715498d
Islam <Ihtyyarovich@gmail.com>
2021-08-14 20:38:18 +0300

    added 3rd task and made changes in readme file

---
+++ src/GlobantAlgo/09.08/09.08.Tests/PredictWinnerTest.cs
@@ -0,0 +1,22 @@
+﻿using PredictWinnerName;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PairGameWinnerName
+{
+    public class PredictWinnerTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 5, 2 }, false)]
+        [InlineData(new int[] { 1, 5, 233, 7 }, true)]
+        public void PredictTheWinnerTest(int[] data, bool expected)
+        {
+            var actual = PairGameWinner.PredictTheWinner(data);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/GlobantAlgo/09.08/PredictWinner/PairGameWinner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictWinnerName
+{
+    public static class PairGameWinner
+    {
+        public static bool PredictTheWinner(int[] nums)
+        {
+            return winner(nums, 0, nums.Length - 1, 1) >= 0;
+        }
+        public static int winner(int[] nums, int s, int e, int turn)
+        {
+            if (s == e)
+                return turn * nums[s];
+            int a = turn * nums[s] + winner(nums, s + 1, e, -turn);
+            int b = turn * nums[e] + winner(nums, s, e - 1, -turn);
+            return turn * Math.Max(turn * a, turn * b);
+        }
+    }
+}
