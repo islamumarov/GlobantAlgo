6f69f01c6dac9fcd27aa50ed7e670927ee70cf9b
Islam <Ihtyyarovich@gmail.com>
2021-09-09 12:58:13 +0300

    init another week

---
+++ src/GlobantAlgo/30.08/30-08-2021/30-08-2021/Stairs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_08_2021
+{
+    public class Stairs
+    {
+        public bool DownStairs(int[] arr)
+        {
+            var ground = arr[-1];
+
+        
+
+            return;
+        }
+    }
+}
