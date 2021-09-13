9d0b906f9c70e34ca5873eb820b23b0d968c59b1
Islam <Ihtyyarovich@gmail.com>
2021-09-13 11:14:44 +0300

    added second and test

---
+++ src/GlobantAlgo/06.09/GreedyAlgorithm/MapWithIslands.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyAlgorithm
+{
+    public class MapWithIslands
+    {
+
+
+        /// <summary>
+//        You are given an m x n binary matrix grid.An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+        /// The area of an island is the number of cells with a value 1 in the island.
+        /// Return the maximum area of an island in grid.If there is no island, return 0
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int MaxAreaOfIsland(int[][] grid){
+            int maxArea = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        int area = 0;
+                        area = GetArea(grid, i, j);
+                        if (area > maxArea)
+                        {
+                            maxArea = area;
+                        }
+                    }
+                }
+            }
+            return maxArea;
+        }
+
+        private int GetArea(int[][] grid, int i, int j)
+        {
+            if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] == 0)
+            {
+                return 0;
+            }
+            grid[i][j] = 0;
+            return 1 + GetArea(grid, i + 1, j) + GetArea(grid, i - 1, j) + GetArea(grid, i, j + 1) + GetArea(grid, i, j - 1);
+        }
+
+        
+
+        public int MaxAreaOfIsland(int[,] grid)
+        {
+            int maxArea = 0;
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] == 1)
+                    {
+                        maxArea = Math.Max(maxArea, AreaOfIsland(grid, i, j));
+                    }
+                }
+            }
+            return maxArea;
+        }
+
+        private int AreaOfIsland(int[,] grid, int i, int j)
+        {
+            if (i < 0 || i >= grid.GetLength(0) || j < 0 || j >= grid.GetLength(1) || grid[i, j] == 0)
+            {
+                return 0;
+            }
+            grid[i, j] = 0;
+            return 1 + AreaOfIsland(grid, i + 1, j) + AreaOfIsland(grid, i - 1, j) + AreaOfIsland(grid, i, j + 1) + AreaOfIsland(grid, i, j - 1);
+        }
+    }
+}
---
+++ src/GlobantAlgo/06.09/GreedyAlgorithmTest/MapWithIslandsTest.cs
@@ -0,0 +1,25 @@
+﻿using GreedyAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GreedyAlgorithmTest
+{
+    public class MapWithIslandsTest
+    {
+        [Fact]
+        public void FindAreaTest()
+        {
+            var map = new MapWithIslands();
+            var matrix1 = new int[,] { {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}  };
+            var expected = 6;
+           // var actual1 = MapWithIslands.countIslands(matrix1);
+            var actual2 = map.MaxAreaOfIsland(matrix1);
+            Assert.Equal<int>(expected, actual2);
+        }
+
+    }
+}
