a4f3d543c902b465ce2a184bb5c6f0ffa59a8bf8
Islam <Ihtyyarovich@gmail.com>
2021-09-15 11:27:53 +0300

    added first task and its unit tests

---
+++ src/GlobantAlgo/Combinatorics/Combinatorics/UniquePath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorics
+{
+    public class UniquePath
+    {
+
+        public static int UniquePaths(int m, int n)
+        {
+            if (n == 1 && m == 1)
+                return 1;
+            if(m < n)
+                return (int)Combination(m+n-2, m-1);
+            return (int)Combination(m + n - 2, n - 1);
+        }
+        public static long  Combination(int n, int m)
+        {
+            long a =1, b =1;
+            while(m > 0)
+            {
+                a *= n--;
+                b *= m--;
+            }
+            return a / b;
+        }
+
+    }
+}
---
+++ src/GlobantAlgo/Combinatorics/CombinatoricsTest/UniquePathsTest.cs
@@ -0,0 +1,24 @@
+﻿using Combinatorics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CombinatoricsTest
+{
+    public class UniquePathsTest
+    {
+
+
+        [Theory]
+        [InlineData(3,7,28)]
+        [InlineData(10,10, 48620)]
+        public void UniquePaths(int m, int n, int expected)
+        {
+            var actual = UniquePath.UniquePaths(m, n);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
